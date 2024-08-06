4175c88c0b47c0c2104ef328cfa7bcd2656fea4b
Rithik sharma <89332289+RIthik-sharma1@users.noreply.github.com>
2024-08-06 22:16:02 +0530

    Create SwitchStatement.java
    This file contains java program for Switch Statement with an example code

---
+++ SwitchStatement.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+/*This is how it works:
+
+The switch expression is evaluated once.
+The value of the expression is compared with the values of each case.
+If there is a match, the associated block of code is executed.
+The break and default keywords are optional, and will be described later in this chapter*/
+
+class SwitchStatement{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Choose a number :");
+        int choice;
+        choice = sc.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Input is invalid");
+        }
+    }
+}
